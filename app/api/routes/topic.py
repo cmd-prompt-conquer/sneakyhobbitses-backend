fcea098ed4677a7efd4bb227b991aaabe6382329
slavkov96 <nikolay.slavkov96@gmail.com>
2024-10-06 13:51:52 +0300

    Add endpoint for all topic stats

--- app/api/routes/topic.py
+++ app/api/routes/topic.py
@@ -72,3 +72,40 @@ async def get_stats(
         "top": qtop,
         "bottom": qbottom,
     })
+
+@router.get("/stats2/{id}")
+async def get_all_stats(session: SessionDep, id: int):
+    topic_stats = [];
+    topics = session.exec(select(Topic)).all()
+    for i, topic in enumerate(topics):
+        topic_stat = await get_topic_stats(session, topic.id)
+        topic_stat["team_name"] = topic.name
+        topic_stats.append(topic_stat)
+
+    return topic_stats
+
+    
+
+async def get_topic_stats(session, id):
+    answers = session.exec(select(Answers).where(Answers.topic_id == id)).all()
+    questions = session.exec(select(Question).where(Question.topic_id == id)).all()
+    stats = {}
+    for i, q in enumerate(questions):
+        for a in answers:
+            ans = a.answers[i]
+            qt = q.question
+            if not stats.get(qt, None):
+                stats[qt] = [0, 0]
+
+            if ans[i] == q.answer:
+                stats[qt][0] += 1
+            else:
+                stats[qt][1] += 1
+
+    qtop = sorted(stats.items(), key=lambda x: x[1][0])[0][0]
+    qbottom = sorted(stats.items(), key=lambda x: x[1][1])[0][0]
+
+    return {
+        "top": qtop,
+        "bottom": qbottom,
+    }
